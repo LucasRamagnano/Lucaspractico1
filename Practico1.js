337f51ce84bf1b1f271a965cfde0be8e1463bdaa
Lucas Ramagnano <lucas.ramagnano@gmail.com>
2022-06-22 20:47:00 -0300

    agregado de array

--- Practico1.js
+++ Practico1.js
@@ -99,3 +99,26 @@ function cancular18cuotas(cuotas) {
     return porcentaje;
 }
 
+
+        function capturar(){
+         function Persona(nombre, edad){
+            this.nombre=nombre;
+            this.edad=edad;
+         }     
+         var nombreCapturar = document.getElementById("nombre").value;
+         var edadCapturar = document.getElementById("edad").value;
+         nuevoSujeto= new Persona(nombreCapturar, edadCapturar);
+         console.log(nuevoSujeto);
+         agregar();
+            }
+        var baseDatos= [];
+        function agregar(){
+    
+        }
+        var BaseDatos= [];
+        function agregar (){
+            baseDatos.push(nuevoSujeto);
+            console.log(baseDatos);
+            document.getElementById("tabla").innerHTML += '<tbody><td>' +nuevoSujeto.nombre+ '</td><td>' +nuevoSujeto.edad+'</td></tbody>' ;
+        }
+    
