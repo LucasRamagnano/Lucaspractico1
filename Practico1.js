caa95c86ae2c5b9cf72bf6e7661f0e9729a5ca46
Lucas Ramagnano <lucas.ramagnano@gmail.com>
2022-06-28 00:08:21 -0300

    cambio var

--- Practico1.js
+++ Practico1.js
@@ -3,122 +3,140 @@ let apellido = prompt("ingrese un apellido");
 let edad = prompt("ingresa tu edad");
 let esMayor = false;
 
-if (edad <= 17 ) {
-    alert("no podes ingresar, sos menor");
-    esMayor = false;
-}
-else {
-    alert("Podes ingresar sos mayor");
-    alert(" bienvenido " + nombre + " " + apellido);
-    esMayor = true;
+if (edad <= 17) {
+  alert("no podes ingresar, sos menor");
+  esMayor = false;
+} else {
+  alert("Podes ingresar sos mayor");
+  alert(" bienvenido " + nombre + " " + apellido);
+  esMayor = true;
 }
 
 if (esMayor == true) {
-    alert('pagina de cotizacion online')
-    let montoPrestamo = parseInt(prompt('Ingrese el valor a solicitar para su prestamo'));
-    let interes3cuotas = 25;
-    let interes6cuotas = 35;
-    let interes12cuotas = 45;
-    let interes18cuotas = 60;
+  alert("pagina de cotizacion online");
+  let montoPrestamo = parseInt(
+    prompt("Ingrese el valor a solicitar para su prestamo")
+  );
+  let interes3cuotas = 25;
+  let interes6cuotas = 35;
+  let interes12cuotas = 45;
+  let interes18cuotas = 60;
 
-    let cuotasPrestamo;
-    while(!cuotasPrestamo || cuotasPrestamo == 0){
-        cuotasPrestamo = parseInt(prompt('Ingrese 3, 6, 12 o 18 cuotas para su prestamo'));
-        switch (cuotasPrestamo) {
-            case 3:
-                alert('Seleccionaste 3 cuotas y vas a tener un interes del: ' + interes3cuotas + '%');
-                console.log('Te van a quedar 3 cuotas de: ' + calcular3cuotas());
-                console.log('Vas a pagar un interes de: ' + calcular3cuotasInteres());
-                break;
-            case 6:
-                alert('Seleccionaste 6 cuotas y vas a tener un interes del: ' + interes6cuotas + '%');
-                console.log('Te van a quedar 6 cuotas de: ' + calcular6cuotas());
-                console.log('Vas a pagar un interes de: ' + calcular6cuotasInteres());
-                break;
-    
-    
-            case 12:
-                alert('Seleccionaste 12 cuotas y vas a tener un interes del: ' + interes12cuotas + '%');
-                console.log('Te van a quedar 12 cuotas de: ' + calcular12cuotas());
-                console.log('Vas a pagar un interes de: ' + calcular12cuotasInteres());
-                break;
-            case 18:
-                alert('Seleccionaste 18 cuotas y vas a tener un interes del: ' + interes18cuotas + '%');
-                console.log('Te van a quedar 18 cuotas de: ' + calcular18cuotas());
-                console.log('Vas a pagar un interes de: ' + calcular18cuotasInteres());
-                break;
-            default:
-                alert('Valor no valido intente nuevamente');
-                cuotasPrestamo = 0;
-                break;
-        }
+  let cuotasPrestamo;
+  while (!cuotasPrestamo || cuotasPrestamo == 0) {
+    cuotasPrestamo = parseInt(
+      prompt("Ingrese 3, 6, 12 o 18 cuotas para su prestamo")
+    );
+    switch (cuotasPrestamo) {
+      case 3:
+        alert(
+          "Seleccionaste 3 cuotas y vas a tener un interes del: " +
+            interes3cuotas +
+            "%"
+        );
+        console.log("Te van a quedar 3 cuotas de: " + calcular3cuotas());
+        console.log("Vas a pagar un interes de: " + calcular3cuotasInteres());
+        break;
+      case 6:
+        alert(
+          "Seleccionaste 6 cuotas y vas a tener un interes del: " +
+            interes6cuotas +
+            "%"
+        );
+        console.log("Te van a quedar 6 cuotas de: " + calcular6cuotas());
+        console.log("Vas a pagar un interes de: " + calcular6cuotasInteres());
+        break;
+
+      case 12:
+        alert(
+          "Seleccionaste 12 cuotas y vas a tener un interes del: " +
+            interes12cuotas +
+            "%"
+        );
+        console.log("Te van a quedar 12 cuotas de: " + calcular12cuotas());
+        console.log("Vas a pagar un interes de: " + calcular12cuotasInteres());
+        break;
+      case 18:
+        alert(
+          "Seleccionaste 18 cuotas y vas a tener un interes del: " +
+            interes18cuotas +
+            "%"
+        );
+        console.log("Te van a quedar 18 cuotas de: " + calcular18cuotas());
+        console.log("Vas a pagar un interes de: " + calcular18cuotasInteres());
+        break;
+      default:
+        alert("Valor no valido intente nuevamente");
+        cuotasPrestamo = 0;
+        break;
     }
-    
-
-
-    
+  }
 }
 
 function calcular3cuotas(cuotas) {
-    porcentaje = (montoPrestamo * 25) / 100;
-    resultado = porcentaje + montoPrestamo;
-    cuotas = resultado / cuotasPrestamo;
-    return cuotas;
+  porcentaje = (montoPrestamo * 25) / 100;
+  resultado = porcentaje + montoPrestamo;
+  cuotas = resultado / cuotasPrestamo;
+  return cuotas;
 }
 function calcular3cuotasInteres(porcentaje) {
-    porcentaje = (montoPrestamo * 25) / 100;
-    return porcentaje;
+  porcentaje = (montoPrestamo * 25) / 100;
+  return porcentaje;
 }
 function calcular6cuotas(cuotas) {
-    porcentaje = (montoPrestamo * 35) / 100;
-    resultado = porcentaje + montoPrestamo;
-    cuotas = resultado / cuotasPrestamo;
-    return cuotas;
+  porcentaje = (montoPrestamo * 35) / 100;
+  resultado = porcentaje + montoPrestamo;
+  cuotas = resultado / cuotasPrestamo;
+  return cuotas;
 }
 function calcular6cuotasInteres(porcentaje) {
-    porcentaje = (montoPrestamo * 35) / 100;
-    return porcentaje;
+  porcentaje = (montoPrestamo * 35) / 100;
+  return porcentaje;
 }
 function calcular12cuotasInteres(porcentaje) {
-    porcentaje = (montoPrestamo * 45) / 100;
-    return porcentaje;
+  porcentaje = (montoPrestamo * 45) / 100;
+  return porcentaje;
 }
 function calcular12cuotas(cuotas) {
-    porcentaje = (montoPrestamo * 45) / 100;
-    resultado = porcentaje + montoPrestamo;
-    cuotas = resultado / cuotasPrestamo;
-    return porcentaje;
+  porcentaje = (montoPrestamo * 45) / 100;
+  resultado = porcentaje + montoPrestamo;
+  cuotas = resultado / cuotasPrestamo;
+  return porcentaje;
 }
 function calcular18cuotasInteres(porcentaje) {
-    porcentaje = (montoPrestamo * 60) / 100;
+  porcentaje = (montoPrestamo * 60) / 100;
 }
 function cancular18cuotas(cuotas) {
-    porcentaje = (montoPrestamo * 60) / 100;
-    resultado = porcentaje + montoPrestamo;
-    cuotas = resultado / cuotasPrestamo;
-    return porcentaje;
+  porcentaje = (montoPrestamo * 60) / 100;
+  resultado = porcentaje + montoPrestamo;
+  cuotas = resultado / cuotasPrestamo;
+  return porcentaje;
 }
 
+const baseDatosPersonas = new Array();
 
-        function capturar(){
-         function Persona(nombre, edad){
-            this.nombre=nombre;
-            this.edad=edad;
-         }     
-         var nombreCapturar = document.getElementById("nombre").value;
-         var edadCapturar = document.getElementById("edad").value;
-         nuevoSujeto= new Persona(nombreCapturar, edadCapturar);
-         console.log(nuevoSujeto);
-         agregar();
-            }
-        var baseDatos= [];
-        function agregar(){
-    
-        }
-        var BaseDatos= [];
-        function agregar (){
-            baseDatos.push(nuevoSujeto);
-            console.log(baseDatos);
-            document.getElementById("tabla").innerHTML += '<tbody><td>' +nuevoSujeto.nombre+ '</td><td>' +nuevoSujeto.edad+'</td></tbody>' ;
-        }
-    
+class Persona {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+}
+
+function capturar() {
+  let nombreCapturar = document.getElementById("nombre").value;
+  let edadCapturar = document.getElementById("edad").value;
+  let nuevaPersona = new Persona(nombreCapturar, edadCapturar);
+  console.log(nuevaPersona);
+  agregar(baseDatosPersonas, nuevaPersona);
+}
+
+function agregar(baseDatos, personaAgregar) {
+  baseDatos.push(personaAgregar);
+  console.log(baseDatos);
+  document.getElementById("tabla").innerHTML +=
+    "<tbody><td>" +
+    personaAgregar.nombre +
+    "</td><td>" +
+    personaAgregar.edad +
+    "</td></tbody>";
+}
